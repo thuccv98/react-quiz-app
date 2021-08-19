eaabbf47936f1cf1a87544d63f69da33eeb1f0b2
thuccv98 <thuccv98@gmail.com>
2021-08-19 17:12:45 +0700

    Initial setup like routing, file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Quiz from './pages/Quiz/Quiz';
+import Result from './pages/Result/Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/quiz" exact>
+            <Quiz />
+          </Route>
+          <Route path="/result" exact>
+            <Result />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div style={{ textAlign: 'center', marginBottom: 10 }}>
+      Made with ❤ by ThucCao
+    </div>
+  );
+};
+
+export default Footer;
